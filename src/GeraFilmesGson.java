58bd2e2877460bd698556d4c55fc48ef72f476d7
ThiagoMullerR <thiago.m.rodrigues@outlook.com>
2022-07-22 00:23:20 -0300

    Primeiro commit

---
+++ src/GeraFilmesGson.java
@@ -0,0 +1,16 @@
+import com.google.gson.Gson;
+
+public class GeraFilmesGson {
+	private String json;
+
+	public GeraFilmesGson(String body) {
+		this.json = body;
+	}
+	
+	public Gson gerar() {
+//		new Gson.formJson;
+		return null;
+		
+	}
+	
+}
---
+++ src/modelo/Filme.java
@@ -0,0 +1,27 @@
+package modelo;
+
+public class Filme {
+
+	private String title;
+	private String image;
+	private double imDbRating;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public double getImDbRating() {
+		return imDbRating;
+	}
+	public void setImDbRating(double imDbRating) {
+		this.imDbRating = imDbRating;
+	}
+}
