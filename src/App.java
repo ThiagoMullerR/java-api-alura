81b970461d5098c5264c9876002fe0a8ea63e5af
ThiagoMullerR <thiago.m.rodrigues@outlook.com>
2022-07-25 11:08:52 -0300

    Aula 3

--- src/App.java
+++ src/App.java
@@ -1,51 +1,39 @@
-import java.lang.reflect.Type;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
-import java.util.Map;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        //Fazer uma conexão http e buscar os 250 filmes
-        // o que o navegador faz:
-        String url = "https://alura-imdb-api.herokuapp.com/movies";
-        URI endereco = URI.create(url);
-        var client =  HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
-        //System.out.println(body);
+	public static void main(String[] args) throws Exception {
+		
+		//Fazer uma conexão http e buscar os 250 filmes
+		// o que o navegador faz:
 
+//		String url = "https://alura-imdb-api.herokuapp.com/movies";
+//		ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
+		
+		String url = "https://api.mocki.io/v2/549a5d8b/NASA-APOD";
+		ExtratorDeConteudo extrator =  new ExtratorDeConteudoDaNasa();
 
-        //Pegar os dados que interessam (titulo, poster, classificação)
-        // parsear - separar
-//        var parser = new JsonParser();
-//        List<Map<String, String>> listaDeFilmes = parser.parse(body);
-        //System.out.println(listaDeFilmes.size());
-        //System.out.println(listaDeFilmes.get(0));
-        
-        Gson gson = new Gson();
-       
-        Type tipoLista = new TypeToken<ArrayList<String>>() {}.getType();
-        List<Map<String, String>> listaDeFilmes = gson.fromJson(body, tipoLista);
-		for(Map<String, String> item : listaDeFilmes)
-		{
-			System.out.println(item);
+		var http = new ClienteHttp();
+		String json = http.buscaDados(url);
+
+		// exibir e manipular os dados
+		List<Conteudo> conteudos = extrator.extraiConteudos(json);
+
+		var geradora = new GeradorDeFigurinhas();
+
+		for(int i=0; i < 3; i++) {
+
+			Conteudo conteudo = conteudos.get(i);
+
+			InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+			String nomeArquivo =  conteudo.getTitulo() + ".png";
+
+			geradora.cria(inputStream, nomeArquivo);
+
+			System.out.println(conteudo.getTitulo());
+			System.out.println();
 		}
 
-        // exibir e manipular os dados
-//        for (Map<String,String> filme : listaDeFilmes) {
-//            System.out.println(filme.get("title"));
-//            System.out.println(filme.get("image"));
-//            System.out.println(filme.get("imDbRating"));
-//            System.out.println();
-//        }
-    }
+	}
 }
---
+++ src/GeradorDeFigurinhas.java
@@ -0,0 +1,40 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public class GeradorDeFigurinhas {
+
+	public void cria(InputStream inputStream, String nomeArquivo) throws Exception {
+		// Leitura da imagem
+//		InputStream inputStream = new FileInputStream("entrada/filme-maior.jpg");
+//		InputStream inputStream = new URL("https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@.jpg").openStream();
+		BufferedImage imagemOriginal = ImageIO.read(inputStream);
+		
+		// cria imagem em memoria com transparencia e tamanho novo
+		int largura = imagemOriginal.getWidth();
+		int altura = imagemOriginal.getHeight();
+		int novaAltura = altura + 200;
+		BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+	
+		// copiar a imagem original para a nova imagem (em memoria)
+		Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+		graphics.drawImage(imagemOriginal, 0, 0, null);
+		
+		// configurar a afonte
+		var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+		graphics.setFont(fonte);
+		
+		// escrever uma frase na imagem
+		graphics.setColor(Color.YELLOW);
+		graphics.drawString("TOPZERA", 100, novaAltura - 100);
+		
+		// escrever a nova imagem em um arquivo
+		ImageIO.write(novaImagem, "png", new File("saida/" + nomeArquivo));
+	}
+	
+}
